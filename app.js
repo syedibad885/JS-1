d9d8a8a426a05e78a78332cd9d99292c45749f29
Syed ibad <155752000+syedibad885@users.noreply.github.com>
2024-02-23 17:50:14 +0500

    JS

---
+++ app.js
@@ -0,0 +1,17 @@
+// Variable
+  let myName = "Syed ibad yasin"; // string
+
+  //text is also called string
+
+  console.log(myName)
+  console.log(myName)
+  console.log(myName)
+  console.log(myName)
+  console.log(myName)
+  console.log(myName)
+  console.log(myName)
+  console.log(myName)
+  console.log(myName)
+  console.log(myName)
+  console.log(myName)
+  console.log(myName)
